02b82bb76d059af5df275fc7788e91c233b5030a
root <root@c0x.fvds.ru>
2016-03-11 13:24:42 -0500

    Returns jpg image now

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import(
     "net/http"
     "log"
     "os"
+    "io/ioutil"
+    "strings"
+    "fmt"
+    "github.com/gorilla/mux"
 )
 
 const resp = `<html>
@@ -20,8 +24,32 @@ func handler(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte(resp))
 }
 
+func pngHandler(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    
+    fmt.Printf("%s\n", vars["item"])
+	
+    if !strings.HasSuffix(vars["item"], ".jpg") {
+            http.Error(w, "Wrong File Extension", http.StatusNotFound)
+        return
+    }
+    file, err := ioutil.ReadFile("/root/work/data/" + vars["item"])
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusNotFound)
+        return
+    }
+    w.Header().Set("Content-type", "image/jpg")
+    w.Write(file)
+}
+
 func main() {
-    http.HandleFunc("/", handler)
+//    http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("/root/work/data"))))
+
+    r := mux.NewRouter()
+    //http.HandleFunc("/", pngHandler)
+    r.HandleFunc("/img/{item}", pngHandler)
+    http.Handle("/", r)
+
     err := http.ListenAndServe(":8080", nil)
 
     if err != nil {
